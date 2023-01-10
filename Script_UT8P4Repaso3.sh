17405cd63181dcc95e30e18ef792dc14750cec0c
Dysforic <94018158+Dysforic@users.noreply.github.com>
2023-01-10 13:49:55 +0100

    Update Script_UT8P4Repaso3.sh

--- Script_UT8P4Repaso3.sh
+++ Script_UT8P4Repaso3.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 	# Un script que busque todos los archivos que contengan la palabra Hack y 
-	# los guarde dentro de una carpeta localizada en /root/cuarentena/fichero.txt
+	# los guarde dentro de una carpeta localizada en /root/cuarentena/
 
-	ls -r ./* | grep -iR "hack" > /root/cuarentena/fichero.txt
-
-
-
+	a=grep -rlnw '/' -e 'hack'
+	echo "$a" > fichero.txt
+	for i in fichero.txt; do
+		cp $i /root/cuartena
+	done
 
 
 
